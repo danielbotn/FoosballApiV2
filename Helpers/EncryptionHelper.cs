7a9755060f0203b6aa6f4b9328a74611c22019ea
Daniel Freyr <danielfrs87@gmail.com>
2024-01-31 21:01:46 +0000

    🐛 Fixed encryption function

--- Helpers/EncryptionHelper.cs
+++ Helpers/EncryptionHelper.cs
@@ -8,15 +8,16 @@ public class EncryptionHelper
     public static string EncryptString(string plainText)
     {
         var hashed = EncryptProvider.Base64Encrypt(plainText);
+        string replacement = "SlashReplacement";
+        hashed = hashed.Replace("/", replacement);
         return hashed;
     }
 
     public static string DecryptString(string encryptedString)
     {
-        var key = Environment.GetEnvironmentVariable("aesKey");
-        var iv = Environment.GetEnvironmentVariable("aesIV");
-        var decryptedString = EncryptProvider.AESDecrypt(encryptedString, key, iv);
-
-        return decryptedString;
+        string replacement = "SlashReplacement";
+        var decryptedString = EncryptProvider.Base64Decrypt(encryptedString);
+        string originalEncodedString = decryptedString.Replace(replacement, "/");
+        return originalEncodedString;
     }
 }
