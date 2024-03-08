47f19ee3c8505486b6d08a5a89791516014a9e4b
Daniel Freyr <danielfrs87@gmail.com>
2024-03-08 09:47:00 +0000

    🔥 Removed unused using statemets

--- Controllers/DoubleLeagueGoalsController.cs
+++ Controllers/DoubleLeagueGoalsController.cs
@@ -1,11 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using AutoMapper;
 using FoosballApi.Dtos.DoubleLeagueGoals;
 using FoosballApi.Services;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FoosballApi.Controllers
