861ec056c2f70dcbe2b5aab17a870034e925a8b0
Daniel Freyr Sigurðsson <danielfrs87@gmail.com>
2024-12-23 22:23:17 +0000

    ✅Modified where clause

--- Services/SingleLeagueMatchService.cs
+++ Services/SingleLeagueMatchService.cs
@@ -255,20 +255,38 @@ namespace FoosballApi.Services
             using (var conn = new NpgsqlConnection(_connectionString))
             {
                 var matches = await conn.QueryAsync<SingleLeagueMatchesQuery>(
-                    @"SELECT slm.id as Id, slm.player_one as PlayerOne, slm.player_two as PlayerTwo, slm.league_id as LeagueId, 
-                    slm.start_time as StartTime, slm.end_time as EndTime,
-                    slm.player_one_score as PlayerOneScore, slm.player_two_score as PlayerTwoScore, 
-                    slm.match_ended as MatchEnded, slm.match_paused as MatchPaused, slm.match_started as MatchStarted,
-                    l.organisation_id as OrganisationId,
-                    (SELECT u.first_name from Users u where u.id = slm.player_one) as PlayerOneFirstName,
-                    (SELECT u2.last_name from Users u2 where u2.id = slm.player_one) as PlayerOneLastName,
-                    (SELECT u3.first_name from Users u3 where u3.id = slm.player_two) as PlayerTwoFirstName,
-                    (SELECT u4.last_name from Users u4 where u4.id = slm.player_two) as PlayerTwoLastName,
-                    (SELECT u5.photo_url from Users u5 where u5.id = slm.player_one) as PlayerOnePhotoUrl,
-                    (SELECT u6.photo_url from Users u6 where u6.id = slm.player_two) as PlayerTwoPhotoUrl
-                    FROM single_league_matches slm
-                    JOIN leagues l on l.id = slm.league_id
-                    where league_id = @league_id",
+                    @"SELECT 
+                        slm.id as Id, 
+                        slm.player_one as PlayerOne, 
+                        slm.player_two as PlayerTwo, 
+                        slm.league_id as LeagueId, 
+                        slm.start_time as StartTime, 
+                        slm.end_time as EndTime,
+                        slm.player_one_score as PlayerOneScore, 
+                        slm.player_two_score as PlayerTwoScore, 
+                        slm.match_ended as MatchEnded, 
+                        slm.match_paused as MatchPaused, 
+                        slm.match_started as MatchStarted,
+                        l.organisation_id as OrganisationId,
+                        (SELECT u.first_name FROM Users u WHERE u.id = slm.player_one) as PlayerOneFirstName,
+                        (SELECT u2.last_name FROM Users u2 WHERE u2.id = slm.player_one) as PlayerOneLastName,
+                        (SELECT u3.first_name FROM Users u3 WHERE u3.id = slm.player_two) as PlayerTwoFirstName,
+                        (SELECT u4.last_name FROM Users u4 WHERE u4.id = slm.player_two) as PlayerTwoLastName,
+                        (SELECT u5.photo_url FROM Users u5 WHERE u5.id = slm.player_one) as PlayerOnePhotoUrl,
+                        (SELECT u6.photo_url FROM Users u6 WHERE u6.id = slm.player_two) as PlayerTwoPhotoUrl
+                    FROM 
+                        single_league_matches slm
+                    JOIN 
+                        leagues l ON l.id = slm.league_id
+                    WHERE 
+                        league_id = @league_id
+                    ORDER BY 
+                        CASE 
+                            WHEN slm.match_ended = true THEN 0
+                            ELSE 1
+                        END,
+                        slm.end_time DESC;
+                    ",
                 new { league_id = leagueId });
                 
                 return matches.ToList();
